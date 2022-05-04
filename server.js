a3a9a3659635b97b796fb9245f00804b6ca7a55c
rahulpra567 <rahul.info567@gmail.com>
2022-05-04 17:22:13 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const swaggerDocument = require('./swagger.json');
+const swaggerUi = require("swagger-ui-express");
+const cors = require("cors");
+const mongoose = require('mongoose');
+const  fs = require('fs');
+const customCss = fs.readFileSync((process.cwd()+"/swagger.css"), 'utf8');
+
+// Router
+const todoRouter = require('./routes/todoRoutes');
+const indexRouter = require('./routes/authRoutes');
+
+const port = 3000;
+const app = express();
+app.use(express.json());
+const mongoDB = 'mongodb://127.0.0.1/todos';
+
+//app.use('/todos',todoRouter);
+app.use(express.urlencoded({extended:false}));
+app.use(cors());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {customCss}));
+app.use('/api',indexRouter);
+app.use('/api/todos/',todoRouter);
+
+
+mongoose.connect(mongoDB, {
+  useNewUrlParser: true, useUnifiedTopology: true
+ }).then(() => {
+  app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`);
+    });
+}).catch(err => {
+   console.log('Could not connect to the database. Exiting now...', err);
+   process.exit();
+});
